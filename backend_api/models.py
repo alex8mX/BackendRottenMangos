c54aa9fdfac0f7ee1ca9aace0c7e1b4158fd8a7f
Alex Alvarado <alexjose9@msn.com>
2022-01-08 12:18:29 -0600

    added models, models to admin, added rest framework and backend api to installed apps

--- backend_api/models.py
+++ backend_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+GENRES_CHOICES = (("Action","Action"),("Comedy","Comedy"),("Drama","Drama"), ("Horror","Horror"),("Romance","Romance"),)
+
+# Create your models here.
+class Movie(models.Model):
+	title = models.CharField(max_length=250)
+	release_date = models.DateField()
+	genre = models.CharField(max_length=250, choices=GENRES_CHOICES)
+	plot = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.title
+
+class Review(models.Model):
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	star_rating = models.IntegerField(null=False, validators = [MaxValueValidator(5),MinValueValidator(0)])
+	comment = models.CharField(max_length=250)
+
+	def __str__(self):
+		return u'%s stars ' % (self.star_rating)
