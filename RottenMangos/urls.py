1a4e3e0355e6853ff026c747cf024d2d39c8da2f
Alex Alvarado <alexjose9@msn.com>
2022-01-12 01:04:17 -0600

    Commented Swagger description and specified swagger auth settings

--- RottenMangos/urls.py
+++ RottenMangos/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
       default_version='v1',
-      description="Test description",
+      description="Backend API for movies and ratings system",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
